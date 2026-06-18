52a9fa6c9d4d556886ab38954f565c1e392f21dd
Poorani R <poorani.r0405@gmail.com>
2026-06-18 13:09:47 +0530

    Fixed placement page and mobile menu

--- script.js
+++ script.js
@@ -12,11 +12,3 @@ document.querySelectorAll(
     });
 
 });
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-if(menuToggle){
-    menuToggle.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-    });
-}
